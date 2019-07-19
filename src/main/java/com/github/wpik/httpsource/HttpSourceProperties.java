646151655b6ae20d3f7a94b61c6340b22edb6648
wpik <wpik256@gmail.com>
2019-07-19 20:25:18 +0200

    Changes required to use source with Spring Cloud Data Flow

--- src/main/java/com/github/wpik/httpsource/HttpSourceProperties.java
+++ src/main/java/com/github/wpik/httpsource/HttpSourceProperties.java
@@ -2,11 +2,11 @@ package com.github.wpik.httpsource;
 
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.integration.http.support.DefaultHttpHeaderMapper;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.cors.CorsConfiguration;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
@@ -16,23 +16,33 @@ import javax.validation.constraints.NotNull;
 @Data
 public class HttpSourceProperties {
 
+    /**
+     * URI path where requests should be handled.
+     * By default: /
+     */
     @NotEmpty
     private String uriPath = "/";
 
+    /**
+     * Array of HTTP methods accepted by the source.
+     * By default: POST
+     */
     @NotEmpty
     private HttpMethod[] httpMethods = {HttpMethod.POST};
 
+    /**
+     * Array of patterns to map HTTP headers into message headers.
+     * By default all common headers (see DefaultHttpHeaderMapper).
+     */
     private String[] mappedRequestHeaders = {DefaultHttpHeaderMapper.HTTP_REQUEST_HEADER_NAME_PATTERN};
 
+    /**
+     * The HTTP response status returned to the client, after successful processing of HTTP request.
+     * By default: ACCEPTED
+     */
     @NotNull
     private HttpStatus responseStatus = HttpStatus.ACCEPTED;
 
-    private CorsProperties cors = new CorsProperties();
-
-    @Data
-    public static class CorsProperties {
-        private String[] allowedOrigins = {CorsConfiguration.ALL};
-        private String[] allowedHeaders = {CorsConfiguration.ALL};
-        private Boolean allowCredentials;
-    }
+    @NestedConfigurationProperty
+    private HttpSourceCorsProperties cors = new HttpSourceCorsProperties();
 }
